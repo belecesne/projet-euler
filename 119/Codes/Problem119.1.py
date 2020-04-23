94cbfef77cb889eb85236a1cb1d4abad7e265ed1
Benjamin <benjamin.lecesne.blc@gmail.com>
2020-04-23 19:07:40 +0200

    merge

--- 119/Codes/Problem119.1.py
+++ 119/Codes/Problem119.1.py
@@ -13,6 +13,7 @@ def sommeChiffreNombre(nombre):
 def A(max):
     index = 0
     nombreAct = 10
+    valeurRetournee = 0
     while index != max:
         decomposition = sommeChiffreNombre(nombreAct)
         k = 2
@@ -21,12 +22,13 @@ def A(max):
                 puiss = decomposition**k
                 if puiss == nombreAct:
                     index += 1
+                    valeurRetournee = nombreAct
                     break
-                elif puiss > nombreAct:
+                if puiss > nombreAct:
                     break
                 k += 1
         nombreAct += 1
-    return puiss
+    return valeurRetournee
 
 
 for nbTeste in range(1,30):
