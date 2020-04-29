35a6061068fb8a8d38b5900751347c3ef47d78a0
tim10banks <lucatrd10@gmail.com>
2020-04-29 18:53:31 +0200

    helpers 119

--- 117/PDFs/Algos/Problem117_3_rapport.py
+++ 117/PDFs/Algos/Problem117_3_rapport.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 def finale(n):
     '''
@@ -10,7 +11,15 @@ def finale(n):
         a, b, c, d = b, c, d, (a+b+c+d)
     return d
 
+def f(n):
+    if n == 0: return [0, 0, 0, 1]
+    else:
+        s = f(n-1)
+        return [s[1], s[2], s[3], s[0] + s[1] + s[2] + s[3]]
+
 nbTeste = int(input("Rentrez le nombre de case disponible : "))
 begin = time.time()
 print("Résultat au problème 117 avec", nbTeste, "comme entrée :", finale(nbTeste))
 print(f"Duration = {time.time() - begin} seconds to complete.")
+
+print(108681103392592 - 52298426843184)
